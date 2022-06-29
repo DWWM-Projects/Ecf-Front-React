17b81d02fa93c20cfcdbe14bc619a8120f251693
Antrâxx <h.julien62@gmail.com>
2022-06-29 10:32:09 +0200

    Api disponible

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import Fiche from "./Fiche.js"
 
 function App() {
+  // constructor(props) {
+  //   super(props);
+
+  // }
+
+  // componentDidMount{
+
+  // }
+
   return (
     <div>
       <div className="navbar">
+        <div className="insideBar">
         <h1>Découvrez notre <span className="blueTitle">Menu.</span></h1>
         <div className="filter">
           <button>Tout</button>
           <button>Viande</button>
           <button>Légumes</button>
         </div>
+        </div>
       </div>
-      <div className="body">
+      <div className="container">
         <Fiche />
       </div>
     </div>
