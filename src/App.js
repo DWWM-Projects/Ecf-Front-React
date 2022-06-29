d3364586bf724871a7397cce35f744c34506fb53
Antrâxx <h.julien62@gmail.com>
2022-06-29 11:25:55 +0200

    Passage props + .map

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
-import Fiche from "./Fiche.js"
+import React from "react";
+import axios from "axios";
+import Fiche from "./Fiche.js";
 
-function App() {
-  // constructor(props) {
-  //   super(props);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: [],
+    }
+  }
 
-  // }
+  componentDidMount() {
+    axios.get("http://localhost:3001/repas").then(response => {
+      this.setState ({dishes: response.data});
+      console.log(response.data);
+    });
+  }
 
-  // componentDidMount{
-
-  // }
-
-  return (
-    <div>
-      <div className="navbar">
-        <div className="insideBar">
-        <h1>Découvrez notre <span className="blueTitle">Menu.</span></h1>
-        <div className="filter">
-          <button>Tout</button>
-          <button>Viande</button>
-          <button>Légumes</button>
+  render() {
+    return (
+      <div>
+        <div className="navbar">
+          <div className="insideBar">
+          <h1>Découvrez notre <span className="blueTitle">Menu.</span></h1>
+          <div className="filter">
+            <button>Tout</button>
+            <button>Viande</button>
+            <button>Légumes</button>
+          </div>
+          </div>
         </div>
+        <div className="container">
+          {this.state.dishes.map(dishe => <Fiche dishe={dishe} key={dishe.id}  />)}
         </div>
       </div>
-      <div className="container">
-        <Fiche />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/Fiche.js
+++ src/Fiche.js
@@ -5,16 +5,16 @@ class Fiche extends React.Component {
     render() {
         return (
             <div className="card">
-                <img src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80'" alt="" />
-                <h3 className="dishName">Mélange de légume</h3>
-                <p className="dishDetails">Salade de tomates et carottes</p>
-                <p className="dishPrice">5.99 €</p>
+                <img src={this.props.dishe.imageSrc} />
+                <h3 className="dishName">{this.props.dishe.title}</h3>
+                <p className="dishDetails">{this.props.dishe.content}</p>
+                <p className="dishPrice">{this.props.dishe.price}</p>
                 <div className="divInfo">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                         <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                     </svg>
-                    <p className="rating">5.0</p>
-                    <p className="review">(87)</p>
+                    <p className="rating">{this.props.dishe.rating}</p>
+                    <p className="review">({this.props.dishe.reviews})</p>
                 </div>
             </div>
         );
