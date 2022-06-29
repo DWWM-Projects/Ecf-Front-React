b099c5cff9b99858efd26e29b6688637b738b8bb
Antrâxx <h.julien62@gmail.com>
2022-06-29 10:14:42 +0200

    Front pour 1 card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import Fiche from "./Fiche.js"
 
 function App() {
   return (
     <div>
-      
+      <div className="navbar">
+        <h1>Découvrez notre <span className="blueTitle">Menu.</span></h1>
+        <div className="filter">
+          <button>Tout</button>
+          <button>Viande</button>
+          <button>Légumes</button>
+        </div>
+      </div>
+      <div className="body">
+        <Fiche />
+      </div>
     </div>
   );
 }
